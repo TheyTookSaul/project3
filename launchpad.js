3ab8bca02f90476c585c8116906472d1425ad6e4
Sual <shern21@uic.edu>
2022-11-29 21:27:15 -0600

    Got all of the chess working as well as launchpad interaction

--- launchpad.js
+++ launchpad.js
@@ -1,49 +1,75 @@
 console.log(navigator);
-let device;
+            let device;
 
-if (navigator.requestMIDIAccess){
-    navigator.requestMIDIAccess().then(success, failure);
-}
-function failure(){
-    console.log("Could not connect MIDI");
-}
-function updateDevices(event){
-    // console.log(event);
-}
+            if (navigator.requestMIDIAccess){
+                navigator.requestMIDIAccess().then(success, failure);
+            }
+            function failure(){
+                console.log("Could not connect MIDI");
+            }
+            function updateDevices(event){
+                // console.log(event);
+            }
 
-function success(midiAccess){
-    midiAccess.addEventListener("statechange", updateDevices);
-    const inputs = midiAccess.inputs;
-    // console.log(inputs)
+            function success(midiAccess){
+                midiAccess.addEventListener("statechange", updateDevices);
+                const inputs = midiAccess.inputs;
+                // console.log(inputs)
+
+                for(let output of midiAccess.outputs.values()){
+                    device = output;
+                    console.log("output selected")
+                }
+
+                inputs.forEach((input) => {
+                    // console.log(input)
+                    input.addEventListener("midimessage", handleInput);
+                })
+            }
+
+            function handleInput(input){
+                // console.log(input);
+                let command = input.data[0];
+                let note = input.data[1];
+                let velocity = input.data[2];
+
+                let data = input.data;
+
+                if (velocity > 0){
+                    console.log(`command: ${command}, note: ${note}, velocity: ${velocity}`);
+                    noteOn(note);
+                }
+
+                if (velocity == 0){
+                    noteOff(note)
+                }
+                
+                if (note == 36){
+                    colorM(note, 10)
+                }
+                
+            }
+
+            function noteOn (note){
+
+                console.log(`note: ${note} // on`)
+
+                if (note == 57){
+                    colorM(note, 90)
+                }
+
+            }
+
+            function noteOff(note){
 
 
-    inputs.forEach((input) => {
-        // console.log(input)
-        input.addEventListener("midimessage", handleInput);
-    })
-}
 
-function handleInput(input){
-    // console.log(input);
-    let command = input.data[0];
-    let note = input.data[1];
-    let velocity = input.data[2];
+            }
 
-    let data = input.data;
+            function colorM(key, clr){
 
-    
+                device && device.send([0x1000,key,clr]);
 
-    if (velocity != 0){
-        console.log(`command: ${command}, note: ${note}, velocity: ${velocity}`);
-        if (note == 38 || note == 50 || note == 62 || note == 72){
-            console.log("D")
-        }
-        if (note == 40 || note == 52 || note == 62 || note == 74){
-            console.log("E")
-        }
-        if (note == 98){
-            window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "blank")
-        }
-    }
-    
-}
+            }
+
+            
